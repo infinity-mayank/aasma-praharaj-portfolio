0ece9324732718b4cdbc7af11e24f496e4517e55
Mayank Sharma <mack.jce02@gmail.com>
2023-04-01 18:26:18 +0530

    Added Basic page.

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import {BsArrowUpRight} from "react-icons/bs";
+
+const onReactOutClick = () => {
+  window.open('https://www.linkedin.com/in/aasma-praharaj-293702114/', '_blank');
+}
+
+export const Home = () => (
+  <div className="flex flex-col items-center justify-center p-4 h-screen">
+    <div className="text-[#836705] font-medium text-5xl">Good things take time. Great things take a little longer</div>
+    <div className="pt-4 text-base font-normal">The secret of getting ahead is getting started - Mark Twain :)</div>
+    <div className="pt-12">
+      <button
+        className="bg-[#FFC800] py-4 px-8 rounded-[116px] w-[175px] font-bold flex items-center"
+        onClick={onReactOutClick}
+      >
+        <BsArrowUpRight />&nbsp;Reach Out
+      </button>
+    </div>
+  </div>
+);
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,7 @@
+export const NavBar = () => (
+  <nav className="bg-white">
+    <div className="flex flex-wrap items-center m-auto p-4 h-36 text-lg justify-center">
+      <span className="font-bold text-xl text-[#FFC800]">Aasma Praharaj</span>&nbsp;| my Work Journal
+    </div>
+  </nav>
+);
